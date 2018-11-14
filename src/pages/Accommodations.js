68d81566bd3befa2b05583a1aec098bd8b2d505d
rynocav226 <rynocav@gmail.com>
2018-11-13 22:55:11 -0500

    started accommodations page

--- src/pages/Accommodations.js
+++ src/pages/Accommodations.js
@@ -1,9 +1,71 @@
 import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import { CardGroup, Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 
 class Accommodations extends Component {
   render() {
     return (
-      <div>ACCOMMODATIONS</div>
+      <Container>
+        <Nav id="accommodationsNavigation" className="justify-content-center">
+          <NavItem>
+            <NavLink href="#aloftCardGroup">Aloft</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#courtyardMarriottCardGroup">Courtyard Marriott</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#doubleTreeCardGroup">DoubleTree SLP</NavLink>
+          </NavItem>
+        </Nav>
+        <div data-spy="scroll" data-target="#accommodationsNavigation" data-offset="0">
+          <CardGroup id="aloftCardGroup">
+            <Card>
+              <CardImg src={require("../assets/aloftImage.jpg")} alt="Card image cap" />
+            </Card>
+            <Card>
+              <CardBody>
+                <CardTitle>Overview</CardTitle>
+              </CardBody>
+            </Card>
+            <Card>
+              <CardBody>
+                <CardTitle>Details</CardTitle>
+              </CardBody>
+            </Card>
+          </CardGroup>
+          <CardGroup id="courtyardMarriottCardGroup">
+            <Card>
+              <CardImg src={require("../assets/courtyardMarriottImage.jpg")} alt="Card image cap" />
+            </Card>
+            <Card>
+              <CardBody>
+                <CardTitle>Overview</CardTitle>
+              </CardBody>
+            </Card>
+            <Card>
+              <CardBody>
+                <CardTitle>Details</CardTitle>
+              </CardBody>
+            </Card>
+          </CardGroup>
+          <CardGroup id="doubleTreeCardGroup">
+            <Card>
+              <CardImg src={require("../assets/doubleTreeImage.jpg")} alt="Card image cap" />
+            </Card>
+            <Card>
+              <CardBody>
+                <CardTitle>Overview</CardTitle>
+              </CardBody>
+            </Card>
+            <Card>
+              <CardBody>
+                <CardTitle>Details</CardTitle>
+              </CardBody>
+            </Card>
+          </CardGroup>
+        </div>
+      </Container>
     );
   }
 }
